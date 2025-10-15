8d548bd6b6bda37327124e4db30c96a1f1401d1a
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-15 22:51:29 +0200

    Add files via upload

---
+++ Ejercicio_3/Program.cs
@@ -0,0 +1,11 @@
+﻿List<int> lista = new List<int> { 2, 4, 4, 6, 8, 2, 10 };
+
+var sinDuplicados = lista.Distinct();
+
+Console.WriteLine("Lista sin duplicados:");
+foreach (var n in sinDuplicados)
+{
+    Console.WriteLine(n);
+}
+int suma = sinDuplicados.Sum();
+Console.WriteLine($"Suma de números únicos: {suma}");
