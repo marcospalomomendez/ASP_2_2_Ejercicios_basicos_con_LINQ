9e74d94022d9cd313dfaa676c3b1e1b15f0588e5
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-15 22:52:47 +0200

    Add files via upload

---
+++ Ejercicio_5/Program.cs
@@ -0,0 +1,10 @@
+﻿List<int> lista2 = new List<int> { 1, 2, 2, 3, 3, 3, 4 };
+
+var grupos = lista2.GroupBy(n => n)
+                   .Select(g => new { Numero = g.Key, Cantidad = g.Count() });
+
+foreach (var g in grupos)
+{
+    Console.WriteLine($"Número: {g.Numero}, Cantidad: {g.Cantidad}");
+
+}
