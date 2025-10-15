833543d2c91d7a117902c131b2a8e236d7e7c0fc
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-15 22:53:41 +0200

    Add files via upload

---
+++ Ejercicio_7/Program.cs
@@ -0,0 +1,13 @@
+﻿List<string> nombres = new List<string> { "Ana", "Luis", "Marta" };
+
+var objetos = nombres.Select(n => new
+{
+    Nombre = n,
+    Longitud = n.Length
+});
+
+foreach (var obj in objetos)
+{
+    Console.WriteLine($"Nombre: {obj.Nombre}, Longitud: {obj.Longitud}");
+}
+  
