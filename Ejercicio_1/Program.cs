52a1e73508a33b32a92133154982b97e89db074d
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-15 22:50:28 +0200

    Add files via upload

---
+++ Ejercicio_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numeros = { 5, 12, 8, 23, 4, 16, 19, 7, 10, 3 };
+
+var mayoresQue10 = numeros.Where(n => n > 10);
+
+Console.WriteLine("Números mayores que 10:");
+foreach (var n in mayoresQue10)
+{
+    Console.WriteLine(n);
+
+}
