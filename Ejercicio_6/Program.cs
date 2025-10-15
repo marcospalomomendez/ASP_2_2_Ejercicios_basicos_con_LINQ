412c656b3acf28912d41f4bd8579e85c2a0d8175
marcospalomomendez <marcos.palomomendez.25@campusfp.es>
2025-10-15 22:53:08 +0200

    Add files via upload

---
+++ Ejercicio_6/Program.cs
@@ -0,0 +1,15 @@
+﻿List<int> lista2 = new List<int> { 1, 2, 2, 3, 3, 3, 4 };
+
+var grupos = lista2.GroupBy(n => n)
+                   .Select(g => new { Numero = g.Key, Cantidad = g.Count() });
+
+foreach (var g in grupos)
+{
+    Console.WriteLine($"Número: {g.Numero}, Cantidad: {g.Cantidad}");
+
+}
+double promedio = lista2
+    .Where(n => n > 1)
+    .Average();
+
+Console.WriteLine($"Promedio de números > 1: {promedio}");
